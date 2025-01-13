59b824c95dd2bbcef30133bcd91e84b6338a28ca
Musakalamz <ogunsolumusa@gmail.com>
2025-01-13 10:16:40 +0100

    fetching cabins data

---
+++ src/services/apiCabins.js
@@ -0,0 +1,14 @@
+import supabase, { supabaseUrl } from "./supabase";
+
+export async function getCabins() {
+  // const { data, error } = await supabase.from("Cabins").select("*");
+
+  const { data, error } = await supabase.from("Cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
